0f31644513da0c6c27f0e3f8301e3c286d2d65e4
Gregor Longariva <gregor.longariva@fau.de>
2023-12-21 19:42:51 +0100

    added new asset images to show warnings in menu

---
+++ Network Share Mounter/model/Notification.swift
@@ -0,0 +1,9 @@
+//
+//  Notification.swift
+//  Network Share Mounter
+//
+//  Created by Longariva, Gregor (RRZE) on 21.12.23.
+//  Copyright © 2023 Regionales Rechenzentrum Erlangen. All rights reserved.
+//
+
+import Foundation
