a5cbd2f6e4277342414b00e94c896904f2245b7b
Gregor Longariva <gregor.longariva@fau.de>
2024-01-05 09:50:28 +0100

    imported some of doge/nomad classes
    to be revised

---
+++ Network Share Mounter/view/KrbAuthViewController.swift
@@ -0,0 +1,9 @@
+//
+//  KrbAuthViewController.swift
+//  Network Share Mounter
+//
+//  Created by Longariva, Gregor (RRZE) on 05.01.24.
+//  Copyright © 2024 Regionales Rechenzentrum Erlangen. All rights reserved.
+//
+
+import Foundation
