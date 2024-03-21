6a83d874f0b045ac5f1a426cbba876043afc4cc2
Gregor Longariva <gregor.longariva@fau.de>
2024-03-21 15:20:31 +0100

    ✨ new trigger for authentication

--- Network Share Mounter/helper/ActivityController.swift
+++ Network Share Mounter/helper/ActivityController.swift
@@ -18,6 +18,7 @@ class ActivityController {
     var mounter: Mounter
     var automaticSignIn: AutomaticSignIn?
     var prefs = PreferenceManager()
+    let appDelegate = NSApplication.shared.delegate as! AppDelegate
     
     init(withMounter: Mounter) {
         mounter = withMounter
@@ -33,11 +34,13 @@ class ActivityController {
         DistributedNotificationCenter.default.removeObserver(self)
         
         // get notification when system sleep is started
-//        NSWorkspace.shared.notificationCenter.addObserver(self, selector: #selector(unmountShares), name: NSWorkspace.willSleepNotification, object: nil)
-//        NSWorkspace.shared.notificationCenter.addObserver(self, selector: #selector(unmountShares), name: NSWorkspace.sessionDidResignActiveNotification, object: nil)
-//        NSWorkspace.shared.notificationCenter.addObserver(self, selector: #selector(mountShares), name: NSWorkspace.didWakeNotification, object: nil)
-//        NSWorkspace.shared.notificationCenter.addObserver(self, selector: #selector(mountShares), name: NSWorkspace.sessionDidBecomeActiveNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(timeGoesBySoSlowly), name: Defaults.nsmTriggerNotification, object: nil)
+        NSWorkspace.shared.notificationCenter.addObserver(self, selector: #selector(unmountShares), name: NSWorkspace.willSleepNotification, object: nil)
+        NSWorkspace.shared.notificationCenter.addObserver(self, selector: #selector(unmountShares), name: NSWorkspace.sessionDidResignActiveNotification, object: nil)
+        NSWorkspace.shared.notificationCenter.addObserver(self, selector: #selector(mountShares), name: NSWorkspace.didWakeNotification, object: nil)
+        NSWorkspace.shared.notificationCenter.addObserver(self, selector: #selector(mountShares), name: NSWorkspace.sessionDidBecomeActiveNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(processAutomaticSignIn), name: Defaults.nsmAuthTriggerNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(timeGoesBySoSlowly), name: Defaults.nsmTimeTriggerNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(mountShares), name: Defaults.nsmMountTriggerNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(unmountShares), name: Defaults.nsmUnmountTriggerNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(userTrigger), name: Defaults.nsmMountManuallyTriggerNotification, object: nil)
         
@@ -63,9 +66,8 @@ class ActivityController {
     
     // call automatic sign in on notification
     @objc func processAutomaticSignIn() {
-        Task {
-            self.automaticSignIn = AutomaticSignIn()
-        }
+        Logger.activityController.debug("processAutomaticSignIn called by CCAPICCacheChangedNotification")
+        appDelegate.automaticSignIn = AutomaticSignIn()
     }
     
     // call mount shares with manually parameter and, if configured, renew kerberos tickets
@@ -81,20 +83,29 @@ class ActivityController {
         }
     }
     
+    /// perform some actions now and then, such as renew Kerberos tickets,
+    /// mount shares etc.
+    ///
+    /// Time goes by so slowly
+    /// Time goes by so slowly
+    /// Time goes by so slowly for those who wait
+    /// No time to hesitate
+    /// Those who run seem to have all the fun
+    /// I'm caught up, I don't know what to do
     @objc func timeGoesBySoSlowly() {
-        Logger.activityController.debug("time passed notification called by Timer:")
+        Logger.activityController.debug("⏰ Time goes by so slowly: got timer notification")
         // run authenticaction only if kerberos auth is enabled
         // forcing unwrapping the optional is OK, since values are "registered"
         // and set to empty string if not set
         // check if a kerberos domain/realm is set and is not empty
         if let krbRealm = self.prefs.string(for: .kerberosRealm), !krbRealm.isEmpty {
-            Logger.activityController.debug("-> Kerberos Realm configured, processing automatic AutomaticSignIn")
+            Logger.activityController.debug(" ▶︎ ...kerberos realm configured, processing AutomaticSignIn")
             self.processAutomaticSignIn()
         }
-        Logger.activityController.debug("-> Check for possible MDM profile changes")
+        Logger.activityController.debug(" ▶︎ ...check for possible MDM profile changes")
         // call updateShareArray() to reflect possible changes in MDM profile
         self.mounter.shareManager.updateShareArray()
-        Logger.activityController.debug("-> Finally call mountAllShares.")
+        Logger.activityController.debug(" ▶︎ ...finally call mountAllShares.")
         Task {
             await self.mounter.mountAllShares()
         }
